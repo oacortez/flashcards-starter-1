ed96c73e919b5a8e252c87f4d6f72ac6539c35b8
Oscar Cortez <oacortez98@gmail.com>
2021-11-29 19:39:16 -0700

    Add describe and as well beforeEach

--- test/Turns-test.js
+++ test/Turns-test.js
@@ -3,3 +3,12 @@ const expect = chai.expect;
 
 const Card = require('../src/Card');
 const Turn = require('../src/Turns');
+
+describe('Turn', () => {
+  
+  let card, turn
+  beforeEach(() => {
+    card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    turn = new Turn('guess', card);
+  });
+});
