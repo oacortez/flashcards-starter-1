431bcc4259eadb723cb93ff9308cc4f9ff8ae345
Oscar Cortez <oacortez98@gmail.com>
2021-11-29 19:47:30 -0700

    Add two starter it blocks

--- test/Turns-test.js
+++ test/Turns-test.js
@@ -11,4 +11,12 @@ describe('Turn', () => {
     card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     turn = new Turn('guess', card);
   });
+
+  it('Should be a function', () => {
+    expect(Turn).to.be.a('function');
+  });
+
+  it('Should be an instance of Turn', () => {
+    expect(turn).to.be.a.instanceof(Turn);
+  });
 });
