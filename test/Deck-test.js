5aaaca16d2ca6e218a1e07ad53f2f52fab15b720
Oscar Cortez <oacortez98@gmail.com>
2021-11-30 17:16:58 -0700

    Add first it block to deck testing file

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Deck = require('..src/Deck');
+const Deck = require('../src/Deck');
 const Card = require('../src/Card');
 const Turn = require('../src/Turns');
 
@@ -13,5 +13,9 @@ describe('Deck', () => {
     card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
     card3 = new Card(3, 'What type of prototype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
     deck = new Deck([card1, card2, card3]);
+  });
+
+  it('Should be a function', () => {
+    expect(Deck).to.be.a('function');
   });
 });
