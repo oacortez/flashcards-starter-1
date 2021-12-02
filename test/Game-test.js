d098c68f92d6a7a2478a8633a2ae039150f484c1
Oscar Cortez <oacortez98@gmail.com>
2021-12-01 21:26:14 -0700

    Add describe block

--- test/Game-test.js
+++ test/Game-test.js
@@ -5,3 +5,18 @@ const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 const Round = require('../src/Round');
 const Game = require('../src/Game');
+
+describe('Game', () => {
+  let card1;
+  let card2;
+  let card3;
+  let game;
+  let deck;
+  let round;
+
+  beforeEach(() => {
+    deck = new Deck([card1, card2, card3]);
+    round = new Round(deck);
+    game = new Game();
+  });
+});
