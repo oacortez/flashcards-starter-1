82f422d7a79c50833ac5864ae05d700704080672
Oscar Cortez <oacortez98@gmail.com>
2021-12-02 14:40:28 -0700

    Add new instance of deck,card, and round for start method

--- src/Game.js
+++ src/Game.js
@@ -1,13 +1,24 @@
-const Card = require('./Card');
-const Deck = require('./Deck');
-const Round = require('./Round');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
 
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = null;
+  }
+
+  start() {
+    const newCards = prototypeQuestions.map(card => {
+      return new Card(card.id, card.question, card.answers, card.correctAnswer);
+    });
+
+    const deck = new Deck(newCards);
+    this.currentRound = new Round(deck);
+  }
 
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
