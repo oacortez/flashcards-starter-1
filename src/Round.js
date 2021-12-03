b3293c6fdfae5e5219837adee168c1a87d231b3c
Oscar Cortez <oacortez98@gmail.com>
2021-12-02 20:15:12 -0700

    Set timer and create function in round class

--- src/Round.js
+++ src/Round.js
@@ -6,6 +6,8 @@ class Round {
     this.currentCard = deck.cards[0];
     this.turns = 0;
     this.incorrectGuesses = [];
+    this.startTime = 0;
+    this.endTime = 0;
   }
 
   returnCurrentCard() {
@@ -28,7 +30,15 @@ class Round {
 
   endRound() {
     console.log(`** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`);
-    return (`** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`);
+    this.timeEnd = Date.now();
+    let durationOfGame = (this.timeEnd -= this.startTime);
+    this.convertTime(durationOfGame);
+  }
+
+  convertTime(time) {
+    var minutes = Math.floor(time / 60000);
+    var seconds = ((time % 60000) / 1000).toFixed(0);
+    console.log(`You've spent ${minutes} minutes and ${(seconds < 10 ? '0' : '')} ${seconds} seconds on this round`);
   }
 }
 
